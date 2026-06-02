8a991e4ebccddae96cc05ad6ece10f0d493f4bab
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-02 14:28:06 +0500

    added the maximum rain function

--- main.py
+++ main.py
@@ -8,19 +8,25 @@ def get_data():
     months = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug"  , "Sep" , "Oct" , "Nov" , "Dec"]
 
     #using numpy
-    final2 = np.random.randint(low=(20,40,0) , high=(45,90,300) , size=(4,12,3))  # 4 ctities 12 moths and 3 readings
+    final2 = np.random.randint(low=(20,40,0) , high=(45,90,300) , size=(4,12,3)) #  4 ctities 12 moths and 3 readings temp , humidity , rainfall 
 
     return final2
 
 def avg(data):
-    all_temp = data [:,:,0]
-    avg = np.round(np.mean(all_temp , 1) , 2)     
+    required_data = data [:,:,0]
+    avg = np.round(np.mean(required_data  , 1) , 2)     
     avg_all =  np.round(np.mean(avg) ,2)
 
     return avg , avg_all
 
+def max_rain(data):
+    required_Data = data [:,:,1]
+    max_rainfall = np.max(required_Data ,1)
+    index = np.argmax(max_rainfall)
+    return max_rainfall , index
+
 def main():
-    temp = get_data()
-    
+    complete_data = get_data()
+
 if __name__ == "__main__":
     main()
