9c18083ab3ea3c432f02f08f24a12b54868ea783
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-02 13:44:17 +0500

    added the function to generate the data

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import numpy as np
 
-def main():
+def get_data():
+    """
+    This function generates the data of 4 cities randomly and then returns that in the form of the numpy array
+    """
     cities = ["Kamra" , "Islamabad" , "Karachi" , "Kohat"]
     months = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug"  , "Sep" , "Oct" , "Nov" , "Dec"]
-    final = []
-    for city in cities:
-        for month in months:
-            temp = np.random.randint(20, 45)
-            humidity = np.random.randint(40, 90)
-            rainfall = np.random.randint(0, 300)
-
-            final.append([city , month , temp , humidity , rainfall])
-
-    print(final)
 
     #using numpy
     final2 = np.random.randint(low=(20,40,0) , high=(45,90,300) , size=(4,12,3))  # 4 ctities 12 moths and 3 readings
-    print(final2)
-    print(final2[0][0]) #kamra janaury temparatur
 
+    return final2
+
+def main():
+    temp = get_data()
 if __name__ == "__main__":
     main()
