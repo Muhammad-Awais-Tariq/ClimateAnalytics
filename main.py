5c42ecb1482393b96847ffe59ade8cd422bf6462
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-02 13:26:54 +0500

    added the normal approach

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import numpy as np
+
 def main():
-    print("Hello from climateanalytics!")
+    cities = ["Kamra" , "Islamabad" , "Karachi" , "Kohat"]
+    months = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug"  , "Sep" , "Oct" , "Nov" , "Dec"]
+    final = []
+    for city in cities:
+        for month in months:
+            temp = np.random.randint(20, 45)
+            humidity = np.random.randint(40, 90)
+            rainfall = np.random.randint(0, 300)
+
+            final.append([city , month , temp , humidity , rainfall])
+
+    print(final)
+
 
 
 if __name__ == "__main__":
